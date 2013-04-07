6150093112633b48f1b05cd283c6d01963996d58
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2013-04-07 13:42:45 -0400

    Updated RunMode to support a flag for running django-debug-toolbar or not.
    Made a fix to the graduation audit template.

--- four_year_plan/run_mode.py
+++ four_year_plan/run_mode.py
@@ -1,13 +1,25 @@
 import os
 
 class RunMode(object):
-    """Convenience class for run-time mode configuration."""
+    """Convenience class for run-time mode configuration.
 
-    def __init__(self, mode=None):
+    If 'mode' is set, it must be one of 'dev' (development), 'stag' (staging), or 'prod'
+    (production).
+
+    If mode is not set, the constructor attempts to set the mode from the run-time path to
+    this file. If the path contains any of the aforementioned mode strings, set the mode
+    accordingly. Otherwise, raise an exception.
+
+    If 'debug_toolbar' is set to a non-null value, set the debug_toolbar property to that
+    value.  Otherwise, set the debug_toolbar property to be true only if in development
+    mode.
+    """
+    def __init__(self, mode=None, debug_toolbar=None):
+        self.debug_toolbar = debug_toolbar
         self._project_path = os.path.realpath(os.path.join(os.path.dirname(__file__),
                                                            os.path.pardir))
 
-        valid_modes = ('dev', 'stage', 'prod')
+        valid_modes = ('dev', 'stag', 'prod')
         if mode is None:
             for m in valid_modes:
                 if m in self._project_path:
@@ -19,6 +31,9 @@ class RunMode(object):
             if mode not in valid_modes:
                 raise RuntimeError("Mode not in '{0}'".format(valid_modes))
             self.mode = mode
+
+        if self.debug_toolbar is None:
+            self.debug_toolbar = self.dev
 
     @property
     def project_path(self):
@@ -35,9 +50,9 @@ class RunMode(object):
         return self.mode == 'dev'
 
     @property
-    def stage(self):
+    def stag(self):
         "Running in staging mode?"
-        return self.mode == 'stage'
+        return self.mode == 'stag'
 
     @property
     def prod(self):
