10bf7215134fe319b9e17701f0961c3f2ce4932f
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2013-04-23 15:25:12 -0400

    Merged tests.py file

--- planner/tests.py
+++ planner/tests.py
@@ -1,11 +1,16 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test.client import Client
+from django.core.urlresolvers import reverse
+
+class HomePageTest(TestCase):
+    def test_home_page(self):
+        client = Client()
+        response = client.get('/')
+        self.assertContains(response, 'iGrad Four Year Planner')
+
+class LoginTest(TestCase):
+    def setUp(self):
+        self.client = Client()
 
 class PlannerTest(TestCase):
     def test_login(self):
@@ -13,3 +18,4 @@ class PlannerTest(TestCase):
         Test basic login.
         """
         login = self.client.login(username='majorStudent', password='password')
+
